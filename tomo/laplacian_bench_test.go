e59018e04404eadecbd03139826cdc8dbcfd1e47
Darkroom4364 <ro.peneder@gmail.com>
2026-04-16 16:55:19 +0200

    Add Laplacian solver benchmark for perf regression testing

---
+++ tomo/laplacian_bench_test.go
@@ -0,0 +1,56 @@
+package tomo_test
+
+import (
+	"testing"
+
+	"github.com/Darkroom4364/netlens/tomo"
+	"github.com/Darkroom4364/netlens/topology"
+)
+
+func BenchmarkLaplacianAutoLambda_Abilene(b *testing.B) {
+	g, err := topology.LoadGraphML(testdataDir() + "/abilene.graphml")
+	if err != nil {
+		b.Fatalf("LoadGraphML: %v", err)
+	}
+	nLinks := g.NumLinks()
+	gt := make([]float64, nLinks)
+	for i := range gt {
+		gt[i] = float64(i+1) * 1.5
+	}
+	p, err := tomo.BuildProblemFromTopology(g, gt)
+	if err != nil {
+		b.Fatalf("BuildProblem: %v", err)
+	}
+	solver := &tomo.LaplacianSolver{} // Lambda=0 triggers auto-select
+	b.ResetTimer()
+	for range b.N {
+		_, err := solver.Solve(p)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkLaplacianAutoLambda_Chinanet(b *testing.B) {
+	g, err := topology.LoadGraphML(testdataDir() + "/chinanet.graphml")
+	if err != nil {
+		b.Fatalf("LoadGraphML: %v", err)
+	}
+	nLinks := g.NumLinks()
+	gt := make([]float64, nLinks)
+	for i := range gt {
+		gt[i] = float64(i+1) * 0.5
+	}
+	p, err := tomo.BuildProblemFromTopology(g, gt)
+	if err != nil {
+		b.Fatalf("BuildProblem: %v", err)
+	}
+	solver := &tomo.LaplacianSolver{}
+	b.ResetTimer()
+	for range b.N {
+		_, err := solver.Solve(p)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
